c9da1574708231ebfb4ddef1fe383983aa4f0c9e
Ajay-css <djalok93068@gmail.com>
2026-02-20 22:24:03 +0530

    gemini version changed

--- server/config/gemini.js
+++ server/config/gemini.js
@@ -4,5 +4,5 @@ import "dotenv/config";
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+  model: "gemini-1.5-flash",
 });
